6ff9849f887f2134a41dfae288f00ba594b75dbf
Eyuel <eyuelworku786@gmail.com>
2023-10-09 16:18:14 +0300

    Add api routes for blogs and basic templates

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const Blog = require("./models/Blog");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blog-app")
+  .catch(console.error.bind(console, "Initial DB connection error:"));
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "DB connection error"));
+db.once("open", () => console.log("Database connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("HOME");
+});
+// INDEX route
+app.get("/blogs", async (req, res) => {
+  const blogs = await Blog.find({});
+  res.render("blogs/index", { blogs });
+});
+// POST route
+app.post("/blogs", async (req, res) => {
+  const blog = new Blog(req.body.blog);
+  await blog.save();
+  res.redirect(`/blogs/${blog.id}`);
+});
+
+app.get("/blogs/new", async (req, res) => {
+  res.render("blogs/new");
+});
+// SHOW route
+app.get("/blogs/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  res.render("blogs/show", { blog });
+});
+// UPDATE route
+app.get("/blogs/:id/edit", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  res.render("blogs/edit", { blog });
+});
+
+app.put("/blogs/:id", async (req, res) => {
+  const blog = await Blog.findByIdAndUpdate(req.params.id, req.body.blog, {
+    new: true,
+  });
+  res.redirect(`/blogs/${blog.id}`);
+});
+// DELETE route
+app.delete("/blogs/:id", async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.redirect("/blogs");
+});
 
 // Put the PORT number in env
 app.listen(3000, () => {
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Blog = require("../models/Blog");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blog-app")
+  .catch(() => console.error.bind(console, "Initial DB connection error:"));
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "DB connection error:"));
+
+db.once("open", console.error.bind(console, "Initial DB connection error:"));
+
+const seedDB = async () => {
+  const blogs = [
+    {
+      title: "How to Successfully Make a Career Switch into AI",
+      content:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis debitis a dolorum eius, delectus distinctio excepturi doloremque omnis vero culpa optio eaque deleniti vel reprehenderit veniam voluptatibus obcaecati quidem molestiae.",
+      image:
+        "https://images.unsplash.com/photo-1677442136019-21780ecad995?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80",
+    },
+    {
+      title: "The art of teleportation",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi asperiores deleniti magnam repellendus accusamus eos sapiente similique, ducimus, facere reiciendis, dolores corporis blanditiis? Aspernatur atque soluta provident eum, quibusdam nobis.",
+      image:
+        "https://plus.unsplash.com/premium_photo-1679082048495-4bc40f9d1a92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+  ];
+
+  await Blog.insertMany(blogs);
+};
+
+seedDB()
+  .then(() => db.close())
+  .catch((err) => console.log("Error:", err.message));
